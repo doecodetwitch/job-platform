31202fc4d0c3b7ad8df0e359fb362499ea6e619b
doecode <doecode@CGM008.local>
2022-10-03 21:51:58 +0200

    account data section - initial

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,13 +1,41 @@
 import type { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
 import Header from '@/src/components/Header/Header';
+import styles from '@/src/styles/account/index.module.css'
 
 const Account: NextPage = () => {
     const { data: session } = useSession({required: true});
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    console.log(session)
+
+    const onSubmit = (data: {}) => {
+        console.log(data)
+    }
     return (
-        <div>
+        <div className={styles.container}>
             <Header />
             <h1>This is your account!</h1>
+            <div className={styles.formContainer}>
+                <h1>Your data</h1>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="inputContainer">
+                            <input placeholder={session?.user?.name || 'your name'} {...register('name', { required: true })} className='input' />
+                            {errors.name && <span className='input-error'>This field is required</span>}
+                        </div>
+                        <div className="inputContainer">
+                            <input placeholder={session?.user?.email || 'your email'} {...register('email', { required: true })} className='input' />
+                            {errors.email && <span className='input-error'>This field is required</span>}
+                        </div>
+                        <div className="inputContainer">
+                            <input placeholder="password" {...register('password', { required: true })} className='input' />
+                            {errors.password && <span className='input-error'>This field is required</span>}
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
