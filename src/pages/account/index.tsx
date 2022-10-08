104aad661ee3e27f34804bf59bd4a4fc58eb2534
doecode <doecode@CGM008.local>
2022-10-08 19:16:07 +0200

    added name to the pet model & displayed pets in account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -20,33 +20,93 @@ const Account: NextPage = () => {
         },
       })
 
-    const onSubmit = (data: any) => {
+    const addPetMutation = trpc.useMutation('account.addPet', {
+        onSuccess: () => {
+            window.location.reload()
+            console.log('success')
+        },
+        onError: (error) => {
+            console.log(`Something went wrong: ${error.message}`)
+        },
+    })
+
+    const myPets = trpc.useQuery(['account.getMyPets']);
+
+    const onNameSubmit = (data: any) => {
         editUsernameMutation.mutate({
             name: data.name
         })
     }
+
+    const onCreatePetSubmit = (data: any) => {
+        addPetMutation.mutate({
+            name: data.petName,
+            type: data.type,
+            breed: data.breed,
+            bio: data.bio,
+            born_at: data.born_at,
+        })
+    }
+
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.formContainer}>
                 <h1>Your data</h1>
                 <div>
-                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <form onSubmit={handleSubmit(onNameSubmit)}>
                         <div className="inputContainer">
                             <input placeholder={session?.user?.name || 'your name'} {...register('name', { required: true })} className='input' />
                             {errors.name && <span className='input-error'>This field is required</span>}
                         </div>
-                        {/* <div className="inputContainer">
-                            <input placeholder={session?.user?.email || 'your email'} {...register('email', { required: true })} className='input' />
-                            {errors.email && <span className='input-error'>This field is required</span>}
-                        </div>
-                        <div className="inputContainer">
-                            <input placeholder="password" {...register('password', { required: true })} className='input' />
-                            {errors.password && <span className='input-error'>This field is required</span>}
-                        </div> */}
                         <button type="submit">Submit</button>
                     </form>
                 </div>
+            </div>
+
+            <div className={styles.formContainer}>
+                {myPets.data?.map((pet) => (
+                    <div>
+                        {pet.name}
+                    </div>
+                ))}
+            </div>
+
+            <div className={styles.formContainer}>
+                <h2>Create a pet profile</h2>
+                <form onSubmit={handleSubmit(onCreatePetSubmit)}>
+                    <div className="inputContainer">
+                        <input placeholder='Name of your pet' {...register('petName', { required: true })} className='input' />
+                        {errors.name && <span className='input-error'>This field is required</span>}
+                    </div>
+                    {/* TODO load available pet types from db */}
+                    <div className="inputContainer">
+                        <select placeholder='Type of your pet' {...register('type', { required: true })} className='input'>
+                            <option value="dog">Dog</option>
+                            <option value="dog">Cat</option>
+                            <option value="dog">Lizard</option>
+                        </select>
+                        {errors.type && <span className='input-error'>This field is required</span>}
+                    </div>
+                    {/* TODO load available breeds based on chosen pet type */}
+                    <div className="inputContainer">
+                        <select placeholder='Breed of your pet' {...register('breed', { required: true })} className='input'>
+                            <option value="dog">Amstaff</option>
+                            <option value="dog">Husky</option>
+                            <option value="dog">Samoyed</option>
+                        </select>
+                        {errors.breed && <span className='input-error'>This field is required</span>}
+                    </div>
+                    <div className="inputContainer">
+                        <textarea placeholder='Bio.. write something about your pet' {...register('bio', { required: false })} className='input' />
+                    </div>
+                    {/* TODO inplement a datePicker */}
+                    <div className="inputContainer">
+                        <input placeholder="Your pet's date of birth" {...register('born_at', { required: true })} className='input' />
+                        {errors.born_at && <span className='input-error'>This field is required</span>}
+                    </div>
+                    <button type="submit">Add a pet</button>
+                </form>
             </div>
         </div>
     );
--- src/server/router/account.ts
+++ src/server/router/account.ts
@@ -1,5 +1,6 @@
 import { createProtectedRouter } from "./context";
 import z from 'zod';
+import { resolve } from "path";
 
 // Example router with queries that can only be hit if the user requesting is signed in
 export const accountRouter = createProtectedRouter()
@@ -17,6 +18,38 @@ export const accountRouter = createProtectedRouter()
         },
       })
 
-      return user
+      return user;
     },
+  }).mutation("addPet", {
+    input: z.object({
+        name: z.string().min(1),
+        type: z.string().min(3),
+        breed: z.string().min(3),
+        bio: z.string().min(20),
+        born_at: z.string().min(1),
+    }),
+    async resolve({ ctx, input }) {
+      const pet = await ctx.prisma.pet.create({
+        data: {
+            name: input.name,
+            type: input.type,
+            breed: input.breed,
+            bio: input.bio,
+            born_at: input.born_at,
+            userId: ctx.session.user.id
+        }
+      });
+
+      return pet;
+    },
+  }).query("getMyPets", {
+    async resolve({ctx}){
+        const pets = await ctx.prisma.pet.findMany({
+            where: {
+                userId: ctx.session.user.id
+            }
+        });
+
+        return pets;
+    }
   });
