b92925e40aa9e02e79276ef87e16b664e391a002
doecode <doecode@CGM008.local>
2022-10-15 13:44:31 +0200

    fixed account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -77,77 +77,27 @@ const Account: NextPage = () => {
 
     return (
         <>
-            <Header />
-            <div className={styles.container}>
-                <div className={styles.formContainer}>
-                    <h1>Your data</h1>
-                    <div>
-                        <form onSubmit={handleSubmit(onNameSubmit)}>
-                            <div className="inputContainer">
-                                <input placeholder={session?.user?.name || 'your name'} {...register('name', { required: true })} className='input' />
-                                {errors.name && <span className='input-error'>This field is required</span>}
-                            </div>
-                            <button type="submit">Submit</button>
-                        </form>
-                    </div>
-                </div>
-
-                <div className={styles.myPetsContainer}>
-                    {myPets.data?.map((pet) => (
-                        <div key={pet.id} className='col-span-4'>
-                            <PetBox pet={pet} />
-                        </div>
-                    ))}
-                </div>
-
-                <div className={styles.formContainer}>
-                    <h2>Create a pet profile</h2>
-                    <form onSubmit={handleSubmitNewPet(onCreatePetSubmit)}>
+        <Header />
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <h1>Your data</h1>
+                <div>
+                    <form onSubmit={handleSubmit(onNameSubmit)}>
                         <div className="inputContainer">
-                            <input placeholder='Name of your pet' {...registerNewPet('petName', { required: true })} className='input' />
-                            {errorsNewPet.petName && <span className='input-error'>This field is required</span>}
+                            <input placeholder={session?.user?.name || 'your name'} {...register('name', { required: true })} className='input' />
+                            {errors.name && <span className='input-error'>This field is required</span>}
                         </div>
-                        <div className="inputContainer">
-                            <input type='file' placeholder='Name of your pet' {...registerNewPet('petImage', { required: true })} className='input' />
-                            {errorsNewPet.petImage && <span className='input-error'>This field is required</span>}
-                        </div>
-                        {/* TODO load available pet types from db */}
-                        <div className="inputContainer">
-                            <select placeholder='Type of your pet' {...registerNewPet('type', { required: true })} className='input'>
-                                <option value="dog">Dog</option>
-                                <option value="cat">Cat</option>
-                                <option value="lizard">Lizard</option>
-                            </select>
-                            {errorsNewPet.type && <span className='input-error'>This field is required</span>}
-                        </div>
-                        {/* TODO load available breeds based on chosen pet type */}
-                        <div className="inputContainer">
-                            <select placeholder='Breed of your pet' {...registerNewPet('breed', { required: true })} className='input'>
-                                <option value="amstaff">Amstaff</option>
-                                <option value="husky">Husky</option>
-                                <option value="samoyed">Samoyed</option>
-                            </select>
-                            {errorsNewPet.breed && <span className='input-error'>This field is required</span>}
-                        </div>
-                        <div className="inputContainer">
-                            <textarea placeholder='Bio.. write something about your pet' {...registerNewPet('bio', { required: false })} className='input' />
-                        </div>
-                        {/* TODO inplement a datePicker */}
-                        <div className="inputContainer">
-                            <input placeholder="Your pet's date of birth" {...registerNewPet('born_at', { required: true })} className='input' />
-                            {errorsNewPet.born_at && <span className='input-error'>This field is required</span>}
-                        </div>
-                        <button type="submit">Add a pet</button>
+                        <button type="submit">Submit</button>
                     </form>
                 </div>
             </div>
 
             <div className={styles.myPetsContainer}>
                 {myPets.data?.map((pet) => (
-                    <div key={pet.id} className='col-span-4'>
-                        <PetBox pet={pet} />
-                    </div>
-                ))}
+                        <div key={pet.id} className='col-span-4'>
+                            <PetBox pet={pet} />
+                        </div>
+                        ))}
             </div>
 
             <div className={styles.formContainer}>
@@ -184,24 +134,72 @@ const Account: NextPage = () => {
                     </div>
                     {/* TODO inplement a datePicker */}
                     <div className="inputContainer">
-                        <DayPicker
-                            fromYear={2005}
-                            toYear={2022}
-                            captionLayout="dropdown"
-                            mode="single"
-                            required
-                            selected={selectedDay}
-                            onSelect={setSelectedDay}
-                        />
+                        <input placeholder="Your pet's date of birth" {...registerNewPet('born_at', { required: true })} className='input' />
+                        {errorsNewPet.born_at && <span className='input-error'>This field is required</span>}
                     </div>
                     <button type="submit">Add a pet</button>
                 </form>
             </div>
         </div>
 
-        <Footer />
+        <div className={styles.myPetsContainer}>
+            {myPets.data?.map((pet) => (
+                    <div key={pet.id} className='col-span-4'>
+                        <PetBox pet={pet} />
+                    </div>
+                    ))}
+        </div>
 
-        </>
+        <div className={styles.formContainer}>
+            <h2>Create a pet profile</h2>
+            <form onSubmit={handleSubmitNewPet(onCreatePetSubmit)}>
+                <div className="inputContainer">
+                    <input placeholder='Name of your pet' {...registerNewPet('petName', { required: true })} className='input' />
+                    {errorsNewPet.petName && <span className='input-error'>This field is required</span>}
+                </div>
+                <div className="inputContainer">
+                    <input type='file' placeholder='Name of your pet' {...registerNewPet('petImage', { required: true })} className='input' />
+                    {errorsNewPet.petImage && <span className='input-error'>This field is required</span>}
+                </div>
+                {/* TODO load available pet types from db */}
+                <div className="inputContainer">
+                    <select placeholder='Type of your pet' {...registerNewPet('type', { required: true })} className='input'>
+                        <option value="dog">Dog</option>
+                        <option value="cat">Cat</option>
+                        <option value="lizard">Lizard</option>
+                    </select>
+                    {errorsNewPet.type && <span className='input-error'>This field is required</span>}
+                </div>
+                {/* TODO load available breeds based on chosen pet type */}
+                <div className="inputContainer">
+                    <select placeholder='Breed of your pet' {...registerNewPet('breed', { required: true })} className='input'>
+                        <option value="amstaff">Amstaff</option>
+                        <option value="husky">Husky</option>
+                        <option value="samoyed">Samoyed</option>
+                    </select>
+                    {errorsNewPet.breed && <span className='input-error'>This field is required</span>}
+                </div>
+                <div className="inputContainer">
+                    <textarea placeholder='Bio.. write something about your pet' {...registerNewPet('bio', { required: false })} className='input' />
+                </div>
+                {/* TODO inplement a datePicker */}
+                <div className="inputContainer">
+                    <DayPicker
+                        fromYear={2005}
+                        toYear={2022}
+                        captionLayout="dropdown"
+                        mode="single"
+                        required
+                        selected={selectedDay}
+                        onSelect={setSelectedDay}
+                    />
+                </div>
+                <button type="submit">Add a pet</button>
+            </form>
+        </div>
+
+    <Footer />
+    </>
     );
 }
 
