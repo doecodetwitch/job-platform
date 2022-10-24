d8e4ed03accdbe28611f8a6ecf6a699028ea3eb1
doecode <doecode@CGM008.local>
2022-10-24 23:13:47 +0200

    myJobs list, list item component, layout style

--- src/server/router/jobs.ts
+++ src/server/router/jobs.ts
@@ -1,7 +1,7 @@
-import { createProtectedRouter } from "./context";
+import { createRouter } from "./context";
 import { resolve } from "path";
 
-export const jobsRouter = createProtectedRouter().query("getAllJobs", {
+export const jobsRouter = createRouter().query("getAllJobs", {
     async resolve({ctx}){
         const jobs = ctx.prisma.job.findMany();
         return jobs;
--- src/server/router/pets.ts
+++ src/server/router/pets.ts
@@ -1,7 +1,7 @@
-import { createProtectedRouter } from "./context";
+import { createRouter } from "./context";
 import { resolve } from "path";
 
-export const petsRouter = createProtectedRouter().query("getAllPets", {
+export const petsRouter = createRouter().query("getAllPets", {
     async resolve({ctx}){
         const pets = ctx.prisma.pet.findMany();
         return pets;
