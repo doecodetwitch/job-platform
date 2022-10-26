28aea8c8d6f3b1ad1439d85259e2d7e2462d953a
=Bartosz Koltoniak <=bartosz.koltoniak@outlook.com>
2022-10-27 01:54:06 +0200

    call to action text and identity providers icons on login page

--- src/components/LoginBox/LoginBox.tsx
+++ src/components/LoginBox/LoginBox.tsx
@@ -4,6 +4,8 @@ import { Provider } from "next-auth/providers"
 import { signIn, SignInOptions } from "next-auth/react"
 import styles from './LoginBox.module.css';
 import logo from "@/src/assets/dog_logo.svg";
+import googleIcon from "@/src/assets/google-icon.svg";
+import discordIcon from "@/src/assets/discord-icon.svg";
 import Image from "next/image";
 
 interface LoginBoxProps {
@@ -20,11 +22,15 @@ const LoginBox = ({providers}: LoginBoxProps) => {
                     <div className={styles.logoName}>
                         doggosy
                     </div>
+                    <div className={styles.callToAction}>
+                        The best doggos are waiting for a date. Quciky check it out!
+                    </div>
                 </div>
                 <div className={styles.loginBoxButtons}>
                     {Object.values(providers).map((provider) => (
                         <div className={styles.loginButton}>
                             <Button size='large' fill='true' priority='mid' onClick={() => signIn(provider.id, { callbackUrl: '/pets' })}>
+                                <img src={provider.id === 'google' ? googleIcon.src : discordIcon.src } className={styles.identityProviderIcon} alt="Identity provider" width='32' />
                                 Sign in with {provider.name}
                             </Button>
                         </div>
