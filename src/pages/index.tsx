23eb9e6a7aa88141e104a6bc10f5f78e35ffb9bf
Jakub Maćkowiak <jakubmackowiak@192.168.1.5>
2022-11-16 21:33:55 +0100

    added Bartas section and scroll to features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,52 @@
 import type { NextPage } from 'next'
 import Header from '@/src/components/Header/Header';
 import Footer from '@/src/components/Footer/Footer';
-import React,{useState} from "react";
+import React,{useEffect, useState, useRef} from "react";
 import FeaturesContent from "@/src/components/FeaturesContent/FeaturesContent";
+import styles from "@/src/styles/homepage/index.module.css"
 
 const HomePage: NextPage = () => {
-const [show,setShow]=useState(true)
+
+const [dogImage, setDogImage] = useState([])
+
+    const featuresSection = useRef (null);
+    const scrollToSection = (elementRef:any) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random/6")
+            .then(res => res.json())
+            .then(data => setDogImage(data.message))
+    }, [])
 
     return (
                 <>
                     <Header />
-                        <div>
-                            <div className="relative bg-blue-600 pt-6 sm:py-16">
-                                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                                    <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-                                        <h2 className="text-3xl text-white sm:text-4xl md:text-5xl">
+                        <div className={styles.main}>
+                            <h1 className={styles.titleText}>Guam viverra orci <span className="text-blue-600">Doggosy</span> eu volutpat  facilisis</h1>
+                            <p className={styles.descriptionText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <div className={styles.buttonDiv}>
+                                <a className={styles.button} onClick={() => scrollToSection(featuresSection)}>Features</a>
+                                <a className={styles.button} href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Watch video</a>
+                            </div>
+                            <div className={styles.imagesDiv}>
+                                {dogImage.map((item, index) => (
+                                    <img className={styles.image} src={item} key={index} />
+                                ))}
+                            </div>
+                        </div>
+                        <div ref={featuresSection}>
+                            <div className={styles.featuresSection}>
+                                <div className={styles.featuresHeader}>
+                                    <div className={styles.featuresText}>
+                                        <h2 className={styles.featuresTitle}>
                                             Everything you need to run your books.
                                         </h2>
-                                        <p className="mt-6 text-lg tracking-tight text-blue-100">
+                                        <p className={styles.featuresContent}>
                                             Well everything you need if you aren’t that picky about minor details like tax compliance.
                                         </p>
                                     </div> 
